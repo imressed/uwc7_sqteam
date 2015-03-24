4d2e7accd7695ddae0de703b589fbb5da1ca639c
imressed <imressed@gmail.com>
2015-03-24 19:10:56 +0200

    add models serializer

--- sqteam/sqapp/serializers.py
+++ sqteam/sqapp/serializers.py
@@ -1,2 +1,27 @@
 # -*- coding: utf-8 -*-
 __author__ = 'imressed'
+
+from django.utils import timezone
+from rest_framework import serializers
+from .models import Project, News, SqUser
+
+class ProjectSerializer(serializers.Serializer):
+    id = serializers.Field()
+    name = serializers.CharField(max_length=100)
+    topic = serializers.CharField(max_length=100)
+    location = serializers.CharField(max_length=100)
+    help_type = serializers.CharField(max_length=100)
+    subscribers = SqUserSerializer(many=True, required=False)
+
+
+class NewsSerializer(serializers.Serializer):
+    id = serializers.Field()
+    date = serializers.DateTimeField(default=timezone.now)
+    text = serializers.CharField(max_length=200)
+    projects = ProjectSerializer(many=True, required=False)
+
+
+class SqUserSerializer(serializers.Serializer):
+    id = serializers.Field()
+    email = serializers.EmailField()
+    projects = ProjectSerializer(many=True, reqired=False)
